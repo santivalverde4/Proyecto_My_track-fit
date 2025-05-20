99868f0b57af0ec40db4a6b6000eadc994ea1123
Dilan Hernandez <dilanhernandez48@gmail.com>
2025-05-19 20:59:58 -0600

    botones statistic

--- My-Track-Fit-ui/app/src/main/java/com/example/my_track_fit/StatisticsFragment.kt
+++ My-Track-Fit-ui/app/src/main/java/com/example/my_track_fit/StatisticsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.my_track_fit
+package com.example.my_track_fit.com.example.my_track_fit
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.my_track_fit.R
 
 class StatisticsFragment : Fragment() {
     override fun onCreateView(
